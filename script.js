83172451872273a31c60a36aacf42daf6e559b2f
RocioTCoria <rociotcoria@gmail.com>
2024-09-03 23:37:06 -0300

    1 Tarea realizada "Error LOGIN"
    Anteriormente el Login.html inicia sesion con solo escribir cualquier cosa en la contraseña, ahora equivale al array de arriba.
    En el caso de escribir una mal salta un mensaje debajo.

--- script.js
+++ script.js
@@ -18,21 +18,41 @@ var usuarios = [
     "rol":2
 
 },
-  {
+{
     "mail":"camimansilo@gmail.com",
     "contraseña":"123",
     "rol":2
-  }
-  
+}
+
 ]
 
 function Ingresar(){
     var mail = document.getElementById("floatingInput").value;
     var contraseña = document.getElementById("floatingPassword").value;
     var login = document.getElementById("login");
+    var Error = document.getElementById("emailError");
+    var Error2 = document.getElementById("contraseñaError");
+
+    if (mail.includes('@')) {
+
+        Error.style.display = 'none';
+
+    } else {
+
+        Error.style.display = 'block';
+    }
+
+    if (contraseña.length<1) {
+
+        Error2.style.display = 'none';
+
+    } else {
+
+        Error2.style.display = 'block';
+    }
 
     for (let index = 0; index < usuarios.length; index++) {
-        if (usuarios[index].contraseña = contraseña && usuarios[index].mail ==  mail  ) {
+        if (usuarios[index].contraseña == contraseña && usuarios[index].mail ==  mail  ) {
             if (usuarios[index].rol==1) {
                 login.setAttribute('action' , 'pagautor.html')
             }
@@ -44,21 +64,7 @@ function Ingresar(){
         }
         
     }
-    alert("Error");
 
 }
-
-// Error de Inicio de Sesion //
-
-function Ingresar () {
-
-    var faltantearroba = document.getElementById("floatingInput").value;
-
-    if (faltantearroba) {
-
-        
-    }
-}
-
 
 
